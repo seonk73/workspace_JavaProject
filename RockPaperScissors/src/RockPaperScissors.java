4296b044264933e2092d7c76d3c9ea9923df4346
Seonk <s2018w31-emirim.hs.kr>
2019-04-16 19:44:51 +0900

    add RockPaperScissors 수정 (%3)

--- RockPaperScissors/src/RockPaperScissors.java
+++ RockPaperScissors/src/RockPaperScissors.java
@@ -40,7 +40,7 @@ public class RockPaperScissors {
 	private static int compare(int p, int c) {
 		// -1 -> 사용자가 졌음, 0 -> 비김, 1 -> 사용자가 이겼음
 //		if((p == 1 && c == 2) || (p == 2 && c == 3) || (p == 3 && c == 1)) {
-		if ((p + 1) % 3 == c) {
+		if ((p + 1) % 3 == c % 3) {
 			return -1;
 //		} else if ((p == 1 && c == 1) || (p == 2 && c == 2) || (p == 3 && c == 3)) {
 		} else if (p == c) {
