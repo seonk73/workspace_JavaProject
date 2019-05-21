9b6650c35af8633cf3f1250c6657534c82ccd02d
Seonk <s2018w31-emirim.hs.kr>
2019-05-21 19:31:01 +0900

    add ImageIcon, JLabel, JButton

---
+++ 20190430_GUI/src/GUITest5_ImageIcon.java
@@ -0,0 +1,35 @@
+import java.awt.Dimension;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUITest5_ImageIcon {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("창이름");
+		JPanel panel = new JPanel();
+		frame.setPreferredSize(new Dimension(400, 600));
+		
+		//ImageIcon, JLabel
+		ImageIcon cat = new ImageIcon("src/images/cat.PNG");
+		JLabel imageLabel = new JLabel(cat);
+		
+		//ImageIcon, JButton
+		ImageIcon cat2 = new ImageIcon("src/images/cat2.PNG");
+		
+		//ImageIcon 크기 수정
+		ImageIcon smallcat2 = new ImageIcon(cat2.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT));
+		JButton imageButton = new JButton(smallcat2);
+		
+		panel.add(imageLabel);
+		panel.add(imageButton);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
